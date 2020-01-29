ab4d442dc156616cf0f3f88092118216bb2297be
Dino Horvat <dxh3401@rit.edu>
2020-01-29 17:04:02 +0100

    matlist

--- apps/book-searcher/src/app/app.module.ts
+++ apps/book-searcher/src/app/app.module.ts
@@ -6,14 +6,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GoogleBooksService } from './services/google-books.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './components/search/search.component';
-import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule } from '@angular/material';
 import { GoogleBooksServiceInterceptor } from './services/google-books.service.interceptor';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, SearchResultsComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule, MatCardModule],
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    SearchResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatListModule
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
